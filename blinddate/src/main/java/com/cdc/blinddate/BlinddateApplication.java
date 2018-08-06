69c854136a2172fb659ffe4ad43b97714034d934
felix <nonaddress@126.com>
2018-08-06 16:20:03 +0800

    搭建过滤器进行字符集设置以及登录验证，开发阶段登录验证可关闭。 2018-08-06 16:19:44

--- blinddate/src/main/java/com/cdc/blinddate/BlinddateApplication.java
+++ blinddate/src/main/java/com/cdc/blinddate/BlinddateApplication.java
@@ -3,9 +3,11 @@ package com.cdc.blinddate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.cdc.blinddate.mapper"})
+@ServletComponentScan//通过注解方式配置过滤器
 public class BlinddateApplication {
 
     public static void main(String[] args) {
--- blinddate/src/main/java/com/cdc/blinddate/controller/UserController.java
+++ blinddate/src/main/java/com/cdc/blinddate/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.cdc.blinddate.controller;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.cdc.blinddate.entity.User;
 import com.cdc.blinddate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
@@ -30,6 +33,12 @@ public class UserController {
         return userService.selectList(null).toString();
     }
 
+    /**
+     * @name: register
+     * @description: 注册接口
+     * @author: 纪佳鸿
+     * @time: 2018/8/6 15:09
+     */
     @RequestMapping("/register")//HttpServletRequest request,HttpServletResponse response
     public String register(@RequestBody Map<String,String> params){
         String registerResult=null;
@@ -46,10 +55,49 @@ public class UserController {
         user.setName(name);
         user.setSex(sex);
         user.setCreateTime(new Date());
-        user.setSex("1");
+        user.setStatus("1");
         userService.insert(user);
         registerResult=user.toString();
         return registerResult;
+    }
+
+    /**
+     * @name: login
+     * @description: 登录接口
+     * @author: 纪佳鸿
+     * @time: 2018/8/6 15:10
+     */
+    @RequestMapping("/login")
+    public String login(@RequestBody Map<String,String> params,HttpServletRequest request){
+        String loginResult=null;
+        String username=params.get("username");
+        String password=params.get("password");
+        Wrapper<User> wapper=new EntityWrapper<User>();
+        wapper.eq("username",username);
+        wapper.eq("password",password);
+        User user=userService.selectOne(wapper);
+        if(null!=user){
+            request.getSession().setAttribute("user",user);
+            user.setLastLoginTime(new Date());
+            userService.updateById(user);
+            loginResult="{\"result\":\"success\"}";
+        }else{
+            loginResult="{\"result\":\"fail\"}";
+        }
+        return loginResult;
+    }
+
+    @RequestMapping("/unlogin")
+    public String unlogin(@RequestBody Map<String,String> params,HttpServletRequest request){
+        String loginResult=null;
+        User user=(User)request.getSession().getAttribute("user");
+        if(null!=user){
+            request.getSession().setAttribute("user",null);
+            loginResult="{\"result\":\"success\"}";
+        }else{
+            loginResult="{\"result\":\"fail\"}";
+        }
+        return loginResult;
     }
 
 }
