a3c0463b181072348de52b6514831094271e68f5
jjh1700 <nonaddress@126.com>
2018-08-10 10:00:54 +0800

    注册逻辑写错了修改过来……2018-08-10 10:00:47

--- blinddate/src/main/java/com/cdc/blinddate/service/impl/UserServiceImpl.java
+++ blinddate/src/main/java/com/cdc/blinddate/service/impl/UserServiceImpl.java
@@ -26,7 +26,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     @Override
     public User register(Map<String, String> params) {
-        User user=new User();
+        User user=null;
         String username=params.get("username");
         Wrapper<User> wrapper=new EntityWrapper<User>();
         wrapper.eq("username",username);
@@ -34,6 +34,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         if(null!=user){
             return null;
         }
+        user=new User();
         String password=params.get("password");
         String name=params.get("name");
         String sex=params.get("sex");
