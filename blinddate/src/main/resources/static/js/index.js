9c04a85aa43f1b8d7f93d11d2fed003a969f6845
Hercoder <athk001@yeah.net>
2018-08-10 11:02:54 +0800

    显示个人资料

--- blinddate/src/main/resources/static/js/index.js
+++ blinddate/src/main/resources/static/js/index.js
@@ -59,4 +59,20 @@ $(document).ready(function () {
 
         }
     })
+
+    // //用户个人资料显示
+    // $("#loginname").click(function () {
+    //     $.ajax({
+    //         url:"/user/getSelf",
+    //         type:"POST",
+    //         dataType:"json",
+    //         cache:false,
+    //         contentType: 'application/json;charset=utf-8',
+    //         async:false,
+    //         data:{"":""},
+    //         success:function (data) {
+    //             alert(data);
+    //         }
+    //     })
+    // })
 });
