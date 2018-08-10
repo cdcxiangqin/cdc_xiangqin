7933c1c8c80585d9160c4c4919a21488dac29435
Hercoder <athk001@yeah.net>
2018-08-10 11:27:59 +0800

    前端资料页面样式修改

---
+++ blinddate/src/main/resources/static/js/userpage.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $.ajax({
+        url:"/user/getSelf",
+        type:"POST",
+        dataType:"json",
+        cache:false,
+        contentType: 'application/json;charset=utf-8',
+        async:false,
+        data:"{}",
+        success:function(data){
+            alert("data")
+        }
+    })
+});
