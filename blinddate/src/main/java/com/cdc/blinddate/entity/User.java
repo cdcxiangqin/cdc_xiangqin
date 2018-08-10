be2ac46d05983c7c01a8ec83e0ca3bb79245b439
jjh1700 <nonaddress@126.com>
2018-08-10 15:42:49 +0800

    增加了星座等字段，但我就是不赋值！2018-08-10 15:42:43

--- blinddate/src/main/java/com/cdc/blinddate/entity/User.java
+++ blinddate/src/main/java/com/cdc/blinddate/entity/User.java
@@ -45,6 +45,15 @@ public class User extends Model<User> {
      * 出生日期
      */
 	private Date birthday;
+	private Integer age;
+    /**
+     * 生肖
+     */
+	private String zodiac;
+    /**
+     * 星座
+     */
+	private String constellation;
     /**
      * 民族
      */
@@ -310,6 +319,30 @@ public class User extends Model<User> {
 
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getZodiac() {
+		return zodiac;
+	}
+
+	public void setZodiac(String zodiac) {
+		this.zodiac = zodiac;
+	}
+
+	public String getConstellation() {
+		return constellation;
+	}
+
+	public void setConstellation(String constellation) {
+		this.constellation = constellation;
 	}
 
 	public String getFolk() {
